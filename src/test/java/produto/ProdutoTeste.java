0d44eadc6a23824ffc6794109f14a31f951900f3
Higor Nucci <higornucci@gmail.com>
2015-09-16 19:56:06 -0400

    testes para carrinho de compra

---
+++ src/test/java/produto/ProdutoTeste.java
@@ -0,0 +1,18 @@
+package produto;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import br.com.htcursos.produto.Produto;
+import br.com.htcursos.produto.ValorInvalido;
+
+public class ProdutoTeste {
+
+	private static final double VALOR_NEGATIVO = -1d;
+
+	@Test(expected = ValorInvalido.class)
+	public void nao_deve_permitir_informar_valor_menor_que_zero() throws Exception {
+		new Produto("Fogão 4 bocas", VALOR_NEGATIVO);
+	}
+}
