05b6ff4099c98c2060ca47c0295dd2cfe5787432
Higor Nucci <higornucci@gmail.com>
2016-02-25 08:00:10 -0400

    pasta com todo o código da apostila

---
+++ codigo-da-apostila/src/main/java/br/com/htcursos/pedido/PedidoHibernateDAO.java
@@ -0,0 +1,12 @@
+package br.com.htcursos.pedido;
+
+import java.util.List;
+
+public class PedidoHibernateDAO implements PedidoRepository {
+
+	@Override
+	public List<Pedido> buscarPelo(int numeroDoPedido) {
+		return null;
+	}
+
+}
