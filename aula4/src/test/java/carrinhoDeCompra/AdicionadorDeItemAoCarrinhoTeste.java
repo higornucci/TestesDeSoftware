8e179087396966ce3740659a69c85cc6e858532a
Higor Nucci <higornucci@gmail.com>
2016-03-11 20:37:38 -0400

    Aula 4 preparada

---
+++ aula4/src/test/java/carrinhoDeCompra/AdicionadorDeItemAoCarrinhoTeste.java
@@ -0,0 +1,37 @@
+package carrinhoDeCompra;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import br.com.htcursos.carrinho.AdicionadorDeItemAoCarrinho;
+import br.com.htcursos.carrinho.CarrinhoHibernateDAO;
+import br.com.htcursos.carrinho.CarrinhoDeCompra;
+import br.com.htcursos.carrinho.CarrinhoRepository;
+import br.com.htcursos.carrinho.ItemDoCarrinho;
+import br.com.htcursos.produto.Produto;
+
+public class AdicionadorDeItemAoCarrinhoTeste {
+	private static final int UM = 1;
+	private Produto celular;
+
+	@Before
+	public void inicializar() throws Exception {
+		double valorUnitario = 1300d;
+		celular = new Produto("Nexus 5", valorUnitario);
+	}
+	
+	@Test
+	public void deve_ser_possivel_adicionar_um_item() throws Exception {
+		ItemDoCarrinho umCelular = new ItemDoCarrinho(celular, UM);
+		CarrinhoDeCompra carrinho = new CarrinhoDeCompra();
+		
+		CarrinhoRepository carrinhoRepository = Mockito.mock(CarrinhoHibernateDAO.class);
+		
+		AdicionadorDeItemAoCarrinho adicionador = 
+					new AdicionadorDeItemAoCarrinho(carrinhoRepository);
+		adicionador.adicionar(umCelular, carrinho);
+		
+		Mockito.verify(carrinhoRepository, Mockito.times(1)).salvar(carrinho);
+	}
+}
